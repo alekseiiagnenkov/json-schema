b0e835890087cc267a2bb6a4b7d3c13f92548ac1
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2022-08-20 12:09:00 +0300

    fix

--- src/main/java/ru/development/jsonschema/TestObject.java
+++ src/main/java/ru/development/jsonschema/TestObject.java
@@ -3,23 +3,30 @@ package ru.development.jsonschema;
 import ru.development.api.dto.object.JsonObjectBoxDto;
 import ru.development.api.dto.object.JsonObjectDto;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestObject {
     public static void main(String[] args) {
-        List<JsonObjectDto> dtoList = new ArrayList<>();
+        List<JsonObjectDto> dtoList = createDtoList();
+        JsonObjectBoxDto dtoBox = createJsonObjectBoxDto(dtoList);
+        System.out.println(dtoBox);
+    }
 
-        for (int i = 0; i < 10; i++) {
-            JsonObjectDto dto = new JsonObjectDto();
-            dto.setSomeProperty1("first " + i);
-            dto.setSomeProperty2("second " + i);
-            dtoList.add(dto);
-        }
-
+    private static JsonObjectBoxDto createJsonObjectBoxDto(List<JsonObjectDto> dtoList) {
         JsonObjectBoxDto dtoBox = new JsonObjectBoxDto();
         dtoBox.setObjectId("someId");
         dtoBox.setObjectList(dtoList);
+        return dtoBox;
+    }
 
-        System.out.println(dtoBox);
+    private static List<JsonObjectDto> createDtoList() {
+        List<JsonObjectDto> dtoList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            JsonObjectDto dto = new JsonObjectDto();
+            dto.setSomeProperty1("first " + i);
+            dtoList.add(dto);
+        }
+        return dtoList;
     }
 }
